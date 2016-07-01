2d30be86114145ea10c04bd94e671127051c8bb4
Gilmar Lima de Souza <Gilmar Lima de Souza>
2016-06-30 22:27:42 -0300

    Finalizei o curso

--- mock/servico/GeradorDePagamento.cs
+++ mock/servico/GeradorDePagamento.cs
@@ -13,6 +13,7 @@ namespace mock.servico
         private Avaliador avaliador;
         private LeilaoDaoFalso leilaoDao;
         private PagamentoDao pagamentoDao;
+        private IRelogio relogio;
 
         public GeradorDePagamento(LeilaoDaoFalso leilaoDao, Avaliador avaliador, 
             PagamentoDao pagamentoDao)
@@ -20,6 +21,16 @@ namespace mock.servico
             this.leilaoDao = leilaoDao;
             this.avaliador = avaliador;
             this.pagamentoDao = pagamentoDao;
+            this.relogio = new RelogioDoSistema();
+        }
+
+        public GeradorDePagamento(LeilaoDaoFalso leilaoDao, Avaliador avaliador, 
+            PagamentoDao pagamentoDao, IRelogio relogio)
+        {
+            this.leilaoDao = leilaoDao;
+            this.avaliador = avaliador;
+            this.pagamentoDao = pagamentoDao;
+            this.relogio = relogio;
         }
 
         public virtual void Gera()
@@ -29,9 +40,29 @@ namespace mock.servico
             foreach (var l in encerrados)
             {
                 avaliador.avalia(l);
-                var pagamento = new Pagamento(this.avaliador.maiorValor, DateTime.Today);
+                var pagamento = new Pagamento(this.avaliador.maiorValor, ProximoDiaUtil());
                 pagamentoDao.Salva(pagamento);
             }
+        }
+
+        public virtual DateTime ProximoDiaUtil()
+        {
+            var data = relogio.Hoje();
+            var diaDaSemana = data.DayOfWeek;
+
+            switch (diaDaSemana)
+            {
+                case DayOfWeek.Sunday:
+                    data = data.AddDays(1);
+                    break;
+                case DayOfWeek.Saturday:
+                    data = data.AddDays(2);
+                    break;
+                default:
+                    break;
+            }
+
+            return data;
         }
     }
 }
--- mock/testes/GeradorDePagamentoTeste.cs
+++ mock/testes/GeradorDePagamentoTeste.cs
@@ -76,6 +76,59 @@ namespace mock.testes
             Assert.AreEqual(250, retorno.valor);
         }
 
+        [Test]
+        public void DeveEmpurrarSabadoParaOProximoDiaUtil()
+        {
+            var leilaoDao = new Mock<LeilaoDaoFalso>();
+            var pagamentoDao = new Mock<PagamentoDao>();
+            var relogio = new Mock<IRelogio>();
+
+            relogio.Setup(r => r.Hoje()).Returns(new DateTime(2016, 07, 02));
+
+            Leilao leilao1 = new Leilao("Playstation");
+            leilao1.propoe(new Lance(new Usuario("Renan"), 500));
+            leilao1.propoe(new Lance(new Usuario("Felipe"), 1500));
+
+            List<Leilao> leiloes = new List<Leilao>();
+            leiloes.Add(leilao1);
+
+            leilaoDao.Setup(l => l.encerrados()).Returns(leiloes);
+
+            Pagamento pagamento = null;
+            pagamentoDao.Setup(p => p.Salva(It.IsAny<Pagamento>())).Callback<Pagamento>(r => pagamento = r);
+
+            GeradorDePagamento gerador = new GeradorDePagamento(leilaoDao.Object, new Avaliador(), pagamentoDao.Object, relogio.Object);
+            gerador.Gera();
+
+            Assert.AreEqual(DayOfWeek.Monday, pagamento.data.DayOfWeek);
+        }
+
+        [Test]
+        public void DeveEmpurrarDomingoParaOProximoDiaUtil()
+        {
+            var leilaoDao = new Mock<LeilaoDaoFalso>();
+            var pagamentoDao = new Mock<PagamentoDao>();
+            var relogio = new Mock<IRelogio>();
+
+            relogio.Setup(r => r.Hoje()).Returns(new DateTime(2016, 07, 03));
+
+            Leilao leilao1 = new Leilao("Playstation");
+            leilao1.propoe(new Lance(new Usuario("Renan"), 500));
+            leilao1.propoe(new Lance(new Usuario("Felipe"), 1500));
+
+            List<Leilao> leiloes = new List<Leilao>();
+            leiloes.Add(leilao1);
+
+            leilaoDao.Setup(l => l.encerrados()).Returns(leiloes);
+
+            Pagamento pagamento = null;
+            pagamentoDao.Setup(p => p.Salva(It.IsAny<Pagamento>())).Callback<Pagamento>(r => pagamento = r);
+
+            GeradorDePagamento gerador = new GeradorDePagamento(leilaoDao.Object, new Avaliador(), pagamentoDao.Object, relogio.Object);
+            gerador.Gera();
+
+            Assert.AreEqual(DayOfWeek.Monday, pagamento.data.DayOfWeek);
+        }
 
 
     }
